8a3c459394c73d73521d531e384e46fe02acf92e
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2020-11-26 06:39:08 +0000

    Add first batch of selection scripts

---
+++ src/05.ps1
@@ -0,0 +1,8 @@
+﻿if (-not (Test-Path devcit-ps2)) {
+    New-Item devcit-ps2 -ItemType Directory
+    Set-Location devcit-ps2
+    # TODO...
+    Get-ChildItem C:\Windows > WinDir.txt
+    # ...
+    Set-Location ..\
+}
