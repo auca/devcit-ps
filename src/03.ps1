229c60ce399a58b3f820c16bbdc7dfd166584b9a
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2020-11-26 05:33:34 +0000

    Upload the first batch of sources

---
+++ src/03.ps1
@@ -0,0 +1,23 @@
+﻿<#
+    Программа считывает два числа и
+    выполняет различные арифметические операции
+    над ними.
+ #>
+
+[single]$num1 = Read-Host "Введите первое число"
+[single]$num2 = Read-Host "Введите второе число"
+
+[single]$res = $num1 + $num2
+Write-Output "$num1 + $num2 = $res"
+
+$res = $num1 - $num2
+Write-Output "$num1 - $num2 = $res"
+
+$res = $num1 * $num2
+Write-Output "$num1 * $num2 = $res"
+
+$res = $num1 / $num2
+Write-Output "$num1 / $num2 = $res"
+
+$res = $num1 % $num2
+Write-Output "$num1 % $num2 = $res"
